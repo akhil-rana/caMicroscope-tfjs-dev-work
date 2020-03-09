14a3a7e9dcce58d27861c6e6d72c389516ed8449
akhilrana0001 <akhilrana0001@gmail.com>
2020-03-10 02:02:57 +0530

    someBasic

--- files/script.js
+++ files/script.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
 
 $("#resetLayers").click(function() {
   // $("#inputShape").val("(60,60,1)");
-  // $("#modelSelect").val(1);
+  // $("#modelSelect").val(1); s
   $("#optimizer").val(0);
   $("#batchSize").val("512");
   $("#epochs").val("20");
